d95165a3f955880ad958283fae41517453e1093f
BilloXD <142104317+BilloXD@users.noreply.github.com>
2024-05-17 14:53:18 +0000

    update

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -1,20 +1,21 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    name: 'owner',
-    description: 'Bot owner info',
-    execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
-        const embed = new EmbedBuilder()
-            .setColor('#FFFFFF')
-            .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
-            .setTimestamp();
+  name: "owner",
+  description: "Bot owner info",
+  execute(message, args) {
+    const DiscordServer = "https://discord.gg/9Tt9KeVkMv";
+    const InstagramLink = "https://www.instagram.com/ig.billo/";
+    const embed = new EmbedBuilder()
+      .setColor("#FFFFFF")
+      .setTitle(" 🫅 Owner Info")
+      .setDescription(
+        `__**About me**__:\n\n <:Wrench:1199404907493609605> *Bot by BilloXD, He is a well know CGI, VFX & 3D artist. He is a manager & Moderator of 10+ Discord Server.  Feel free to contact him!* \n <:dc:1199403812096589925> **Discord Support Server : [BilloXD](${DiscordServer})** \n <:instagram:1199403542847434843> **Instagram : [ig.billo](${InstagramLink})**`,
+      )
+      .setTimestamp();
 
-
-        message.reply({ embeds: [embed] });
-    },
+    message.reply({ embeds: [embed] });
+  },
 };
 
 /*
